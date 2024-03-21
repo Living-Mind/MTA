ff6b7f4787e9e8ff4fe60e66e991ad0dc079c460
Stanislav.Chavdarov <redlined_twick@aleeas.com>
2024-03-21 19:50:48 +0200

    second commit

---
+++ string_formating.py
@@ -0,0 +1,5 @@
+first_name = str(input("Type your first name: "))
+last_name = str(input("Type your last name: "))
+amount = float(input("How much money do you have?: "))
+
+print("Hello " + first_name + " " + last_name + ", you have $" + str(amount) + " in your account.")
